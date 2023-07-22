1236042bd9850ecb672e6f52b96f0ffce26fd9d6
KodeJaROM <138987362+KodeJaROM@users.noreply.github.com>
2023-07-22 06:54:04 -0500

    Added log command

--- Version Control System/task/src/svcs/Main.kt
+++ Version Control System/task/src/svcs/Main.kt
@@ -19,12 +19,14 @@ fun main(args: Array<String>) {
     vcsDir.mkdir()
     val usernameFile = File(vcsDir, "config.txt")
     val indexFile = File(vcsDir, "index.txt")
+    val logFile = File(vcsDir, "log.txt")
 
     if (input.isEmpty() || input == "--help") {
         println("These are SVCS commands:")
         for ((key, value) in helpList)
             println("$key $value")
-    } else if (helpList.containsKey(input) && input != "config" && input != "add") {
+//        This else if will need to be removed after implementing the last command
+    } else if (helpList.containsKey(input) && input == "checkout") {
         println(helpList[input])
 
 //  Implementing the config command and creating a vcs folder to store username
@@ -59,6 +61,18 @@ fun main(args: Array<String>) {
             indexFile.appendText("$track\n")
             println("The file '$track' is tracked.")
         }
+
+//  Implementing the log command: If log text exists print it, otherwise print "No commits yet."
+    } else if (input == "log") {
+        if (logFile.exists()) {
+            val log = logFile.readText()
+            println(log)
+        } else {
+            println("No commits yet.")
+        }
+// Implementing the commit command
+    } else if () {
+
     } else {
         println("'$input' is not a SVCS command.")
     }
